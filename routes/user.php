5d1953100ab98ffe0026c8ebc1b6ca7bddf99714
M_Qattan <mohamad99qattan@gmail.com>
2023-05-22 00:09:12 +0300

    user routes changed

--- routes/user.php
+++ routes/user.php
@@ -36,3 +36,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class,'login']);
 Route::post('logout',[UserController::class,'logout'])->middleware('auth:user-api');
+
+// changed
